422cd49cd80adb284bb05414e911a54a819400ae
Learath <learath2@gmail.com>
2020-06-12 23:53:41 +0300

    Get /spec working

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -3246,9 +3246,10 @@ void CServer::SnapFreeID(int ID)
 }
 
 
+
 void *CServer::SnapNewItem(int Type, int ID, int Size)
 {
-	if(!(Type >= 0 && Type <= 0xffff))
+	if(Type > 0xffff)
 	{
 		g_UuidManager.GetUuid(Type);
 	}
